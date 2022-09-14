7c2560262ed97917b8a96c206728e95695dced97
Luis <luisgmsn2001@gmail.com>
2022-09-14 13:06:55 -0500

    Cambio en estilo de fuente y background en el navbar

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,23 @@ import 'package:flutter/material.dart';
 void main() {
   var app = MaterialApp(
     home: Scaffold(
-        appBar: AppBar(
-      title: const Text(
-        "Mi primera aplicación 190292",
-        style: TextStyle(fontFamily: 'Pacifico'),
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        title: const Text(
+          "Mi primera aplicación 190292",
+          style: TextStyle(fontFamily: 'Pacifico', color: Colors.blueGrey),
+        ),
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+
+        //Image.asset("assets/images/bulb.jpg")
       ),
-      backgroundColor: Colors.amber,
-      //Image.asset("assets/images/bulb.jpg")
-    )),
+      body: Container(
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('assets/wall.png'), fit: BoxFit.fill)),
+      ),
+    ),
   );
   runApp(app);
 }
